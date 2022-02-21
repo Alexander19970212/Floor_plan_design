829989c09def4d8518602e211452e705c5bcbab2
Alexander19970212 <kalexu97@mail.ru>
2022-02-21 22:32:18 +0300

    Density function added

--- SpaceOptimize/envoirement.py
+++ SpaceOptimize/envoirement.py
@@ -4,6 +4,8 @@ from abc import abstractmethod
 from traceback import walk_stack
 from turtle import window_width
 from typing import Protocol, List
+import torch
+
 
 @dataclass
 class Point:
@@ -33,22 +35,22 @@ class OptimizedObject(Protocol):  # Проработать API объекта
         pass
 
 
-class GroupObject(): # расчет лосса для группы, так и между группами
+class GroupObject():  # расчет лосса для группы, так и между группами
     """
         Все интересующие параметры будут извлекаться из групп
         Произвести операции с коордианатами объектов, принадлежащих группе
         Габариты объектов - матрица габаритов
         Освещенность - вектор для группы, для объекта число
     """
+
     def __init__(self, objects: List[OptimizedObject]):
         pass
-
-
 
 
 class Table(OptimizedObject):
     def get_coords(self) -> List[Point]:
         return super().get_coords()
+
 
 t = Table()
 
@@ -58,13 +60,12 @@ p = c[0]
 p.y
 
 
-
-
 class Area:
     """
     Получить освещенность окон. (Координаты, Ширина окон, Осввещенность, Нормаль) - для одной границы. 
     Для всей Area отдавать матрицу освещенности помещения, как surface
     """
+
     def __init__(self, walls: List[Wall], windows: List[Window], *, surface=None):
         self.walls = walls
         self.windows = windows
@@ -75,5 +76,22 @@ class Area:
     def get_surface_lumen(self):
         pass
 
-    
+
+def density(group):
+    """
+    Function returns sum of distances between points and centre of points' cloud.
+    :param group: object with Group type.
+    :return: float (sum of distance) and torch vector as distant list
+    """
+
+    coordinates = group.get_parameters("coordinates")  # torch tensor with size (batch_size, N_objects, 2) !!!! COPY
+    centre = torch.unsqueeze(torch.mean(coordinates*1.0, 1), 1)
+    distant = torch.sqrt(torch.sum(torch.square(coordinates - centre), 2))
+
+    return distant.sum(1), distant, distant.sum()
+
+def
+
+
+
 
